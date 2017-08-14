65a126bf5651bf6df18b40531b6aa3c5e0c10501
Anoop Singh <>
2017-08-14 13:11:10 +0530

    added image to the readme.md

--- Example/app.js
+++ Example/app.js
@@ -81,15 +81,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
