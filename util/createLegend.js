6ec204256ce2e56700685b44ef62c0b22526e27c
Macbook <anoop100singh@gmail.com>
2017-11-26 14:47:03 +0530

    added circle type legend

--- util/createLegend.js
+++ util/createLegend.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-import { Rect, G, Text } from 'react-native-svg'
+import { Rect, G, Text, Circle } from 'react-native-svg'
 import _ from 'lodash'
 
-export default function createLegend(legendColor,legendText,width,margin,legendStyle){
+export default function createLegend(legendColor,legendText,width,margin,legendStyle,circleLegendType){
   let marginLegend = -100
-  var legendGroup = _.map(legendColor, (d,i) => {
+  var legendGroup
+  if(!circleLegendType)
+  {
+   legendGroup = _.map(legendColor, (d,i) => {
                     marginLegend += 100;
                   return <G key= {i}>
                            <Rect 
@@ -24,8 +27,25 @@ export default function createLegend(legendColor,legendText,width,margin,legendS
                              fill= {legendStyle.legentTextFill}>
                              {legendText[i]}
                             </Text>
-                         </G> 
-  })
+                         </G>
+   })
+  }
+  else {
+    legendGroup = _.map(legendColor, (d,i) => {
+      marginLegend += 100;
+    return <G key= {i}>
+             <Circle key={'circle_' + i} strokeWidth={2.5} stroke= {legendColor[i]} fill= {'white'} cx= {((width)/legendColor.length+marginLegend- 35)/1.3} cy= {margin.top-15} r={4} />
+             <Text
+              textAnchor= 'middle'
+              fontSize= {legendStyle.legendFontSize}
+              x= {((width)/legendColor.length+marginLegend-3)/1.3}
+              y= {margin.top-24}
+              fill= {legendStyle.legentTextFill}>
+              {legendText[i]}
+             </Text>
+           </G> 
+    })
+  } 
 
   return legendGroup
 }
